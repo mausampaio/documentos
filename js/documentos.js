15d28cce4a9420d1e3e4d900f9e5a4bbb5578d7c
Mauricio Sampaio <mausampaio@gmail.com>
2020-01-27 21:24:55 -0300

    Adicionando botão voltar

--- js/documentos.js
+++ js/documentos.js
@@ -1,8 +1,14 @@
 function search() {
     var cpf = document.getElementById('inputCPF').value;
-    console.log(cpf);
     validation(cpf);
 }
 function validation(cpfValue) {
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
+}
+function back() {
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('inputCPF').value = '';
 }
