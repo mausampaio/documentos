3834787e443b9070550d662b15d8c3de0507596f
Mauricio Sampaio <mausampaio@gmail.com>
2020-01-28 20:48:48 -0300

    Busca da pasta no Firebase por CPF

--- js/documentos.js
+++ js/documentos.js
@@ -3,10 +3,25 @@ function search() {
     validation(cpf);
 }
 function validation(cpfValue) {
+    var storage = firebase.storage();
+    //Retorna uma promisse que será processada 
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        if(todosArquivos.items.length >= 1){
+            next(cpfValue);
+        } else {
+            alert('CPF não encontrado');
+        }        
+    }).catch(function(error){
+        console.log('ERRO', error);
+    });
+}
+
+function next(cpfValue){
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
     document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
 }
+
 function back() {
     document.getElementById('busca').removeAttribute("class", "ocultar");
     document.getElementById('resultado').setAttribute("class", "ocultar");
