543bd5d9e033c06179cf62266cd9fdc636ee08ec
Mauricio Sampaio <mausampaio@gmail.com>
2020-01-27 21:41:25 -0300

    Conectando com firebase

---
+++ js/initFirebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBnTN5eL_2iFhPNA67ouLyThE5lh-03E7U",
+    authDomain: "documentos-aeb08.firebaseapp.com",
+    databaseURL: "https://documentos-aeb08.firebaseio.com",
+    projectId: "documentos-aeb08",
+    storageBucket: "documentos-aeb08.appspot.com",
+    messagingSenderId: "928551128940",
+    appId: "1:928551128940:web:d2731b6b25bc11f52fad49"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
