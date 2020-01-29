6836c687348e9dc83fb651f95c536b8408985a03
Mauricio Sampaio <mausampaio@gmail.com>
2020-01-28 21:54:39 -0300

    Buscando URL do arquivo do Firebase

--- js/documentos.js
+++ js/documentos.js
@@ -2,11 +2,13 @@ function search() {
     var cpf = document.getElementById('inputCPF').value;
     validation(cpf);
 }
+
 function validation(cpfValue) {
     var storage = firebase.storage();
     //Retorna uma promisse que será processada 
     storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
         if(todosArquivos.items.length >= 1){
+            listFiles(cpfValue);
             next(cpfValue);
         } else {
             alert('CPF não encontrado');
@@ -16,10 +18,26 @@ function validation(cpfValue) {
     });
 }
 
-function next(cpfValue){
+function listFiles(cpfValue) {
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
+    var storage = firebase.storage();
+    var files;
+    var fileNames = [];
+    var fileLinks = [];
+    storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        files = todosArquivos.items;
+        for(let i = 0; i<files.length; i++){
+            fileNames.push(files[i].name);
+            storage.ref(cpfValue+'/'+fileNames).getDownloadURL().then(function(url){
+                document.getElementById('certificado').innerHTML = '<a href="'+url+'" target="_blank" rel="noopener noreferrer">Certificado</a>'
+            });            
+        }
+    });
+}
+
+function next(cpfValue) {
     document.getElementById('busca').setAttribute("class", "ocultar");
     document.getElementById('resultado').removeAttribute("class", "ocultar");
-    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: ' + cpfValue;
 }
 
 function back() {
